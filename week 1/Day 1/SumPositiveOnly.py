b1a23370b9448b3e989e8172d58550f62e2fd1c8
nattygy <nathnaelakale@gmail.com>
2021-01-01 18:44:27 +0300

    sum positive only

---
+++ week 1/Day 1/SumPositiveOnly.py
@@ -0,0 +1,22 @@
+def SumPositiveOnly(a, b):
+    carry = 0
+    res = ""
+    summ = 0
+    num1 = a[::-1]
+    num2 = b[::-1]
+
+    app = abs(len(a) - len(b))
+    if len(a) > len(b):
+        num2 = num2 + ("0" * app)
+    else:
+        num1 = num1 + ("0" * app)
+
+    for i in range(len(a)):
+        summ = int(num1[i]) + int(num2[i])
+        res = res + (str((summ % 10) + carry))
+        carry = summ // 10
+
+    return res[::-1]
+
+
+print(SumPositiveOnly("1239", "123"))

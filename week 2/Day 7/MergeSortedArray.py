36eb870536466abb6739a72ae48667b911805d94
nattygy <nathnaelakale@gmail.com>
2021-01-16 10:45:49 +0300

    merge sorted array finished

---
+++ week 2/Day 7/MergeSortedArray.py
@@ -0,0 +1,39 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        head = None
+        tail = None
+        while l1 and l2:
+            if l1.val <= l2.val:
+                node = head
+                if node is None:
+                    head = ListNode(l1.val)
+                    tail = head
+                else:
+                    tail.next = ListNode(l1.val)
+                    tail = tail.next
+                l1 = l1.next
+            else:
+                node = head
+                if node is None:
+                    head = ListNode(l2.val)
+                    tail = head
+                else:
+                    tail.next = ListNode(l2.val)
+                    tail = tail.next
+                l2 = l2.next
+        if l1 != None:
+            if head == None:
+                head = l1
+            else:
+                tail.next = l1
+        if l2 != None:
+            if head == None:
+                head = l2
+            else:
+                tail.next = l2
+        return head
